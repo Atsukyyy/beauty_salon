291371350be80b42e4ac4962675c9ca9dc88effc
AtsukiMitsuyori <a.mitsuyori.15@gmail.com>
2018-08-07 01:41:21 +0900

    固定ページ追加

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ActionController::Base
   def top
 
   end
+
+  def about
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'pages#top'
+  get 'about', to: 'pages#about'
 end
